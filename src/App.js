86e431e58a695c979799e2b89dee5dcf0a75611d
Kyler Marquis <cryptojedi@MacBook-Pro-2.local>
2022-09-27 10:50:00 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+//darkside (contrast)
+//halflife (contrast)
+//solarflare (contrast)
+//yitzkok (contrast)
